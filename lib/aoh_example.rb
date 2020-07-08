4f05c7b088fc7cbcda3841dd86d86ecb342f1e55
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-08 03:35:37 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,8 +25,9 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
- new_value = { name:  "Joelle van Dyne (PGOAT)", occupation: "Radio Personality" }
-  
+
+aoh[row][key] = new_value
+  aoh
   
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
